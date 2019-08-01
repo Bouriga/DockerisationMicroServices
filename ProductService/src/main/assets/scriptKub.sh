ab90b96c7a1fc9fdd294ad9324ed5bf1c04b2e2d
Bouriga <bourigaali@mail.com>
2019-08-01 16:32:17 -0400

    first commit

--- ProductService/src/main/assets/scriptKub.sh
+++ ProductService/src/main/assets/scriptKub.sh
@@ -3,8 +3,8 @@ kubectl create configmap postgres-config --from-literal=postgres.service.name=po
 kubectl get cm postgres-config -o json
 kubectl create secret generic db-security --from-literal=db.user.name=alibouriga --from-literal=db.user.password=password
 kubectl get secret db-security -o json
-kubectl create -f https://github.com/Bouriga/DockerisationMicroServices/tree/master/ProductService/src/main/assets/postgres.yaml --validate=false
-kubectl create -f https://github.com/Bouriga/DockerisationMicroServices/tree/master/ProductService/src/main/assets/postgresService.yaml --validate=false
+kubectl create -f https://github.com/Bouriga/DockerisationMicroServices/tree/master/ProductService/src/main/assets/postgres.yaml
+kubectl create -f https://github.com/Bouriga/DockerisationMicroServices/tree/master/ProductService/src/main/assets/postgresService.yaml
 kubectl get deployment postgresql -o json
 kubectl get service postgresql -o json
 kubectl get svc postgresql
