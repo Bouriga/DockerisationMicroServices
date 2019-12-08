8259d380fbf4c079cf0e9fdcc19e5d41bc483b30
Bouriga <bourigaali@mail.com>
2019-12-08 10:53:44 -0500

    first commit

--- myboutique-config-server/src/main/java/com/targa/labs/myboutique/configserver/ConfigserverApplication.java
+++ myboutique-config-server/src/main/java/com/targa/labs/myboutique/configserver/ConfigserverApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigserverApplication {
-    /*test pipeline push */
+
     public static void main(String[] args) {
         SpringApplication.run(ConfigserverApplication.class, args);
     }
